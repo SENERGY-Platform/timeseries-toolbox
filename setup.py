4200e550b3701f2701f0978f7f74d547a89e1df2
Hannes Hansen <mail@hannesh.de>
2023-07-21 15:01:58 +0200

    added requirements

--- setup.py
+++ setup.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python
 
 import setuptools
+from pathlib import Path
+
+def get_install_requires():
+    """Returns requirements.txt parsed to a list"""
+    fname = Path(__file__).parent / 'requirements.txt'
+    targets = []
+    if fname.exists():
+        with open(fname, 'r') as f:
+            targets = f.read().splitlines()
+    return targets
 
 setuptools.setup(
       name='timeseries-toolbox',
@@ -10,4 +20,5 @@ setuptools.setup(
       author_email='',
       packages=setuptools.find_packages(),
       python_requires='>=3.5.3',
+      install_requires=get_install_requires()
 )
