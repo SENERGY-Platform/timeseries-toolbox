68cc4806cbbc3ddc6650217add5f2d4a35e70e5e
Hannes Hansen <mail@hannesh.de>
2024-04-05 13:23:25 +0200

    fixed boto read and write

--- toolbox/data/loaders/s3/s3.py
+++ toolbox/data/loaders/s3/s3.py
@@ -8,12 +8,12 @@ class S3DataLoader():
         aws_access_key_id, 
         aws_secret_access_key
     ):
-        self.s3 = boto3.resource('s3', endpoint_url=s3_url, aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
+        self.s3 = boto3.client('s3', endpoint_url=s3_url, aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
 
     def get_data(self, bucket_name, object_key):
-        data = self.s3.Bucket(bucket_name).get_object(object_key)
-        return pickle.loads(data)
+        data = self.s3.get_object(Bucket=bucket_name, Key=object_key)
+        return pickle.loads(data['Body'].read())
 
     def put_data(self, bucket_name, file_name, data):
-        self.s3.Bucket(bucket_name).put_object(Key=file_name, Body=pickle.dumps(data))
+        self.s3.put_object(Bucket=bucket_name, Key=file_name, Body=pickle.dumps(data))
 
