2fe1a8e1a8940b1750cda23d4ba0a4d5b8d2cff9
Hannes Hansen <mail@hannesh.de>
2024-05-22 16:47:16 +0200

    fixed imports

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def get_install_requires():
 
 setuptools.setup(
       name='timeseries-toolbox',
-      version='2.1.0',
+      version='2.1.1',
       description='ML Toolbox',
       author='Hannes Hansen',
       author_email='',
