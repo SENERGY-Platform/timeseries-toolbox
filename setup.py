7f8149b0ce4dad1dca87ec229832596fa27f52bf
Hannes Hansen <mail@hannesh.de>
2024-05-27 10:53:57 +0200

    added earliest offset to kafka loader

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def get_install_requires():
 
 setuptools.setup(
       name='timeseries-toolbox',
-      version='2.1.25',
+      version='2.1.26',
       description='ML Toolbox',
       author='Hannes Hansen',
       author_email='',
