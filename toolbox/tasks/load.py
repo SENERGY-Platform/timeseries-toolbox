bd448936fb7228c16bdeafb8ef16f68e125f280e
Hannes Hansen <mail@hannesh.de>
2024-05-29 15:33:03 +0200

    fixed small bug with task setup

--- toolbox/tasks/load.py
+++ toolbox/tasks/load.py
@@ -1,8 +1,9 @@
 from toolbox.tasks.fit_model.fit import Fit
 from toolbox.tasks.load_shifting.run import LoadShifting
+from toolbox.ml_config import Config
 
-def get_task(task_name, task_settings):
+def get_task(task_name, config: Config):
     if task_name == 'ml_fit':
-        return Fit(task_settings)
+        return Fit(config.task_settings, config.MLFLOW_URL, config.EXPERIMENT_NAME, config.USER_ID, config.COMMIT)
     elif task_name == 'load_shifting':
-        return LoadShifting(task_settings)
+        return LoadShifting(config.task_settings)
