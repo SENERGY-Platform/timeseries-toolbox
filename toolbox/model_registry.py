eaffe69e54877358043fd6f27194b96da9691450
Hannes Hansen <mail@hannesh.de>
2024-05-31 12:56:17 +0200

    add model alias

--- toolbox/model_registry.py
+++ toolbox/model_registry.py
@@ -2,8 +2,14 @@ import json
 
 import mlflow
 import mlflow.pyfunc
+from mlflow import MlflowClient
 
 def store_model(model_artifact, userid, config, job_name, tool_box_version, metrics, data_settings):
+    # This will store the model at MLFlow model registry
+    # If it does not exist, it will be created with version 1
+    # All following models will increment the version by 1
+    # The latest version gets the alias `production`
+
     print('store model')
 
     mlflow.end_run()
@@ -27,7 +33,10 @@ def store_model(model_artifact, userid, config, job_name, tool_box_version, metr
         "data_settings": json.dumps(data_settings)
     }
     tags.update(config)
-    mlflow.register_model(model_uri, job_name, tags=tags)
+    created_model_version = mlflow.register_model(model_uri, job_name, tags=tags)
+    client = MlflowClient()
+    client.set_registered_model_alias(job_name, "production", created_model_version.version)
+
 
 def load_model(model_name,version, mlflow_url):
     mlflow.set_tracking_uri(mlflow_url)
