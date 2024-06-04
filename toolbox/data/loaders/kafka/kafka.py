638ff926d5de5b3ca09b7c181df936522d6ccdd0
Hannes Hansen <mail@hannesh.de>
2024-06-04 13:35:31 +0200

    added log

--- toolbox/data/loaders/kafka/kafka.py
+++ toolbox/data/loaders/kafka/kafka.py
@@ -90,7 +90,7 @@ class KafkaLoader(DataLoader):
         
         unnesting_stream_name = self.create_unnesting_stream()
         stream_name = self.create_stream(unnesting_stream_name)
-        time.sleep(60) # Unfortunately without this random sleep, the select query will be empty. I guess that KSQL is not ready even though the requests return successfully 
+        time.sleep(600) # Unfortunately without this random sleep, the select query will be empty. I guess that KSQL is not ready even though the requests return successfully 
         select_query = self.build_select_query(stream_name, self.topic_config.time_range_value, self.topic_config.time_range_level)
         result = self.query_data(select_query)
         
