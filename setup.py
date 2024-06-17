8b2f832f9092df9964a9eb54a1b37467a84d7077
Hannes Hansen <mail@hannesh.de>
2024-06-17 11:10:21 +0200

    small fix

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def get_install_requires():
 
 setuptools.setup(
       name='timeseries-toolbox',
-      version='2.2.55',
+      version='2.2.56',
       description='ML Toolbox',
       author='Hannes Hansen',
       author_email='',
--- toolbox/peak_shaving/use_case.py
+++ toolbox/peak_shaving/use_case.py
@@ -1,10 +1,9 @@
 from .pipeline import PeakShavingPipeline
-from toolbox.timeseries.series import convert_df_to_series
 
 class PeakShavingUseCase():
     def fit(self, data, train_config, model_name):
         pipeline = PeakShavingPipeline()
-        data = list(convert_df_to_series(data))
+        data = list(data)
         pipeline.fit(data)
         return pipeline, {}, None
 
