9e896b31c55a9e88ec4f6cd4ff88bd2ccb762125
c-schrade <schrade.christoph@gmail.com>
2025-08-13 15:52:50 +0200

    bump mlflow

--- setup.py
+++ setup.py
@@ -22,9 +22,9 @@ setuptools.setup(
       python_requires='>=3.9.0',
       install_requires=get_install_requires(),
       extras_require={
-          "anomaly_detection": ["scikit-learn==1.5.0", "torch==2.3.1", "mlflow==2.11.1"],
-          "peak_shaving": ["scipy<2", "mlflow==2.11.1", "KDEpy==1.1.13"],
-          "load_shifting": ["scipy==1.13.1", "ray==2.24.0", "mlflow==2.11.1"],
+          "anomaly_detection": ["scikit-learn==1.5.0", "torch==2.3.1", "mlflow==3.2.0"],
+          "peak_shaving": ["scipy<2", "mlflow==3.2.0", "KDEpy==1.1.13"],
+          "load_shifting": ["scipy==1.13.1", "ray==2.24.0", "mlflow==3.2.0"],
           "estimation": ["darts==0.24.0", "torch==2.0.1", "gluonts==0.13.2"],
           "data": ["ksql-query-builder @ git+https://github.com/SENERGY-Platform/ksql-query-builder", "boto3==1.34.77", "httpx[http2]==0.27.0"]
       }
